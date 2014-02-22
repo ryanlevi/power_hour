c0ef897f24f76d17ee2905c2893e4f10022b8f1f
Ryan <ryan.e.levi@gmail.com>
2014-02-22 13:43:09 -0800

    changed to 20 songs

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,7 +5,7 @@ class HomeController < ApplicationController
 
   def show
     @song = GetSong.new(params[:query])
-    @similar = @song.get_similar(59)
+    @similar = @song.get_similar(19)
   end
 
   def about
@@ -14,7 +14,7 @@ class HomeController < ApplicationController
   def random
     artists = ["kanye west", "alicia keys", "beyonce", "sublime", "kendrick lamar"]
     @song = GetSong.new(artists[rand(artists.length-1)])
-    @similar = @song.get_similar(59)
+    @similar = @song.get_similar(19)
     render 'show'
   end
 end
