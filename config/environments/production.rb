4374778a7dd8b44e491e413a4a30252b53832611
Ryan <ryan.e.levi@gmail.com>
2014-02-22 14:49:25 -0800

    d

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,14 +22,12 @@ PowerHour::Application.configure do
   # Disable Rails's static asset server (Apache or nginx will already do this).
   config.serve_static_assets = true
 
-  config.action_dispatch.x_sendfile_header = ‘X-Accel-Redirect’
-
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = true
+  config.assets.compile = false
 
   # Generate digests for assets URLs.
   config.assets.digest = true
