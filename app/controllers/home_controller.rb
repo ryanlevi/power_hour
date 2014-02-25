ff888b8a37b663fb66ec870ce75f9a9350462fdb
Ryan <ryan.e.levi@gmail.com>
2014-02-24 16:18:54 -0800

    tweaked random so that user cnever gets same playlist twice in a row

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -18,7 +18,12 @@ class HomeController < ApplicationController
 
   def random
     cookies[:limit] ||= 60
-    @song = GetSong.new(Queries.offset(rand(Queries.count)).first.query)
+    random = Queries.offset(rand(Queries.count)).first.query
+    @song = GetSong.new(random)
+    while @song.title == random
+      random = Queries.offset(rand(Queries.count)).first.query
+      @song = GetSong.new(random)
+    end
     @similar = @song.get_similar(cookies[:limit].to_i-1)
     render 'show'
   end
@@ -26,7 +31,13 @@ class HomeController < ApplicationController
   def settings
     cookies[:limit] ||= 60
     if params[:limit]
-      cookies[:limit] = params[:limit].to_i
+      if params[:limit].to_i >= 120
+        cookies[:limit] = 120
+      elsif params[:limit].to_i < 1
+        cookies[:limit] = 1
+      else
+        cookies[:limit] = params[:limit].to_i
+      end
     end
   end
 end
